08a6d71be5b137df361ff8c9d50e93e61081eb74
gjz010 <gjz010944@gmail.com>
2024-07-16 01:31:05 +0800

    feat: FILE* example

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 hello.bin: hello.c
 	x86_64-unknown-linux-musl-gcc -O3 hello.c -static-pie -fpie -o hello.bin
-.phony: clean
+objdump: hello.bin
+	objdump -D hello.bin > hello.bin.dump
+.phony: clean objdump
 clean:
-	rm -f hello.bin
+	rm -f hello.bin test.txt
--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char** argv){
     printf("Hello, world!\n");
     for(int i=0; i<argc; i++){
         printf("argv[%d] = %s\n", argc, argv[i]);
     }
+    FILE* f = fopen("test.txt", "w");
+    const char* s = "Hello, world!\n";
+    fwrite(s, sizeof(char), 14, f);
+    fclose(f);
     return 0;
 }
